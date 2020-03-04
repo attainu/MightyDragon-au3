528ab9ce8effc12b56edf2b7b5ebe84e837ccb25
Ravi Kumar Vykuntham <rvykuntham@gmail.com>
2020-03-05 00:53:20 +0530

    changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,45 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { Router, Switch, Route, Redirect } from 'react-router-dom';
+import { render } from 'react-dom';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+import history from './history';
+import Root from './components/Root';
+import AccountDragons from './components/AccountDragons';
+import PublicDragons from './components/PublicDragons';
+import { fetchAuthenticated } from './actions/account';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
 
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk)
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const AuthRoute = props => {
+  if (!store.getState().account.loggedIn) {
+    return <Redirect to={{ pathname: '/' }} />
+  }
+
+  const { component, path } = props;
+
+  return <Route path={path} component={component} />;
+}
+
+store.dispatch(fetchAuthenticated())
+  .then(() => {
+    render(
+      <Provider store={store}>
+        <Router history={history}>
+          <Switch>
+            <Route exact path='/' component={Root} />
+            <AuthRoute path='/account-dragons' component={AccountDragons} />
+            <AuthRoute path='/public-dragons' component={PublicDragons} />
+          </Switch>
+        </Router>
+      </Provider>,
+      document.getElementById('root')
+    );
+  });
