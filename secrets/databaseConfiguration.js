7180c33ae5c6f3fe692c70cd5806ee2671677459
Ravi Kumar Vykuntham <rvykuntham@gmail.com>
2020-03-09 23:02:49 +0530

    git push origin master

--- secrets/databaseConfiguration.js
+++ secrets/databaseConfiguration.js
@@ -1,7 +1,7 @@
 module.exports = {
-  user: "node_user",
-  host: "localhost",
-  database: "dragonstackdb",
-  password: "node_password",
+  user: "kdbncdccwmbept",
+  host: "ec2-3-213-192-58.compute-1.amazonaws.com",
+  database: "d2msf0uq3hirh8",
+  password: "7a5d26928be4860fde875ff9909d4584dbde6e0971f2b5130709ed6af87c6ac8",
   port: 5432
 };
